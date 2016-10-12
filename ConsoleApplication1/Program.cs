b6f65647687379f59b2b336cb4ce6f52b9a1d973
josemalave167 <josemalave167@gmail.com>
2016-10-12 12:47:37 +0100

    new

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace PrimerNumbers
 {
     class Program
     {
+        void prime_num(long num)
+        {
+           
+            for (int i = 0; i <= num; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j <= num; j++)
+                {
+                    if (i != j && i % j == 0 || i == 1)  
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    
+                    Console.WriteLine(i);
+                }
+                isPrime = true;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Prime numbers between 1 and 1,000:");
+            Program p = new Program();
+            p.prime_num(1000);
+            Console.ReadLine();
         }
     }
 }
